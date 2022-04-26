3a68ac9900d554038b401c0769dccca85987aa12
julesy <julesssie.dev@gmail.com>
2022-04-25 20:32:50 -0400

    UX/UI rewrite for 2022 version

--- webapp_main/forms.py
+++ webapp_main/forms.py
@@ -1,10 +1,29 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    q=forms.CharField(label='Search', max_length=100)
+    q=forms.CharField(max_length=100, label='')
 
     #update the widget(html code rendered on the page) with specific values. 
-    q.widget.attrs.update({'autocomplete':'off'})
+    q.widget.attrs.update(
+        {
+            'autocomplete':'off',
+            'placeholder':'Search Articles',
+            'class': 'form-control'
+        }
+    )
 
 class DateForm(forms.Form):
-    q=forms.DateField(widget = forms.DateInput(attrs={'type':'date'}))
+    q=forms.DateField(
+        widget = forms.DateInput(
+            attrs = {
+                'type':'date'
+            }
+        ),
+        label=''
+    )
+
+    q.widget.attrs.update(
+        {
+            'class': 'form-control'
+        }
+    )
