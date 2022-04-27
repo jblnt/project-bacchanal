24bd3fb296405670166585c5b73d234c0af7deaf
julesy <julesssie.dev@gmail.com>
2022-04-26 22:05:14 -0400

    Updated UX/UI and refactored code to better event handling

--- webapp_main/urls.py
+++ webapp_main/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from . import views
-#from .views import ArticleDetailView
-
 from datetime import datetime
 
 urlpatterns = [
     path('', views.newsListView, name='news-home'),
     path('archive/', views.archiveListView, name='archive'),
-    path('search/', views.SearchView, name='global-search'),
-    path('search/<str:query>/<str:topic>/', views.SearchView_Filter, name='global-search-filter'),
     path('article/<int:pk>-<slug:slug>/', views.ArticleDetailView.as_view(), name='article-detail'),
-
-    path('sources/<str:source>/', views.source, name='source'),
-    path('sports/<path:source>/<str:topic>/', views.sportsListView, name='category-sports')
+    path('search/', views.SearchView, name='global-search'),
+    path('source/<str:source>/', views.source, name='source'),
+    path('tags/', views.tagView, name='tags')
 ]
--- webapp_main/views.py
+++ webapp_main/views.py
@@ -15,24 +15,22 @@ from django.db.models import Q
 from .models import Articles
 from .forms import SearchForm, DateForm
 
+# Helper Functions
 def day():
-    delta=-5
+    delta = -5
     now = datetime.now(tz=timezone(timedelta(hours=delta)))
-    return date(now.year, now.month, now.day)
-    #return datetime(2020, 3, 17)
+    #return date(now.year, now.month, now.day)
+    return datetime(2022, 4, 25)
 
-# Create your views here.
-
+# Function Based Views
+# Non Premium Functions
 def newsListView(request):
-    article_list=Articles.objects.filter(tag__icontains="News", date=day())
-
+    article_list=Articles.objects.filter(tag__icontains = "News", date = day())
     paginator=Paginator(article_list, 8)
-    p_num=request.GET.get('page')
-    page_obj=paginator.get_page(p_num)
+    pageNum = request.GET.get('page') if request.GET.get('page') else 1
+    page_obj = paginator.get_page(pageNum)
 
-    base=request.path.split('/')[1]
-    if base == '':
-        base='all'
+    source = request.path.split('/')[1] if request.path.split('/')[1] != "" else "all"
 
     context={
         'articles':page_obj.object_list,
@@ -41,136 +39,117 @@ def newsListView(request):
         'form':SearchForm(),
         'form_date': DateForm(),
         'page_obj':page_obj,
+        'source': source
+    }
 
-        'base': base
+    return render(request, 'webapp_main/articles_list.html', context)
+
+def source(request, source):
+    article_list = Articles.objects.filter(source__icontains=source, date=day())
+    paginator = Paginator(article_list, 8)
+    p_num = request.GET.get('page') if request.GET.get('page') else 1
+    page_obj = paginator.get_page(p_num)
+
+    context = {
+        'articles':page_obj.object_list,
+        'title':source.upper(),
+        'topic':source + ' news',
+        'page_obj':page_obj,
+        'source': source
+    }
+
+    return render(request, 'webapp_main/articles_list.html', context)
+
+def tagView(request):
+    source = request.GET.get("source")
+    tag = request.GET.get("tag")
+
+    excludedItems = Articles.objects.exclude(Q(tag__icontains = "News")|Q(tag__icontains = "Sports")|Q(tag__icontains = "Letters"))
+
+    if source == "all":
+        article_list = Articles.objects.filter(tag__icontains = tag, date = day()) if tag != "others" else excludedItems.filter(date = day()).order_by('tag')
+    else:
+        article_list = Articles.objects.filter(source__icontains = source, tag__icontains = tag, date = day()) if tag != "others" else excludedItems.filter(source__icontains = source, date = day()).order_by('tag')
+
+    paginator = Paginator(article_list, 8)
+    p_num = request.GET.get('page') if request.GET.get('page') else 1
+    page_obj = paginator.get_page(p_num)
+
+    #eg txt3 = "My name is {}, I'm {}".format("John",36) 
+    context={
+        "articles" : page_obj.object_list,
+        "title" : source.upper(),
+        "topic" : source + ' - ' + tag,
+        "page_obj" : page_obj,
+        "source" : source,
+        "query": "source={}&tag={}".format(source, tag)
+    }
+
+    return render(request, 'webapp_main/articles_list.html', context)
+
+@login_required
+def SearchView(request):
+    requestedQuery = request.GET.get("q")
+    requestedTag = request.GET.get("tag")
+
+    excludedItems = Articles.objects.exclude(Q(tag__icontains = "News")|Q(tag__icontains = "Sports")|Q(tag__icontains = "Letters"))
+
+    if requestedTag:
+        article_list =  Articles.objects.filter(title__icontains = requestedQuery, tag__icontains = requestedTag) if requestedTag != "others" else excludedItems.filter(title__icontains = requestedQuery).order_by('tag')
+        queryString = "q={}&tag={}".format(requestedQuery, requestedTag)
+    else:
+        article_list=Articles.objects.filter(title__icontains = requestedQuery).order_by('source')
+        queryString = "q={}".format(requestedQuery)
+
+    paginator=Paginator(article_list, 8)
+    p_num = request.GET.get('page') if request.GET.get('page') else 1
+    page_obj=paginator.get_page(p_num)
+
+    context={
+        'articles':page_obj.object_list,
+        'title': request.GET["q"]+' Results',
+        'topic':'Results For "'+request.GET["q"]+'"',
+        'page_obj': page_obj,
+        'source': 'query',
+        'q': requestedQuery,
+        'query' : queryString
     }
 
     return render(request, 'webapp_main/articles_list.html', context)
 
 @login_required
 def archiveListView(request):
-    if request.method == 'GET':
-        article_list=Articles.objects.filter(date=request.GET["q"]).order_by('source')
+    requestedDate = request.GET.get("q")
+    requestedTag = request.GET.get("tag")
 
-        paginator=Paginator(article_list, 8)
-        p_num=request.GET.get('page')
-        page_obj=paginator.get_page(p_num)
+    excludedItems = Articles.objects.exclude(Q(tag__icontains = "News")|Q(tag__icontains = "Sports")|Q(tag__icontains = "Letters"))
 
-        context={
-            'articles':page_obj.object_list,
-            'title': request.GET["q"]+' Stories',
-            'topic':'News From '+request.GET["q"],
-            #'form':SearchForm(),
-            #'form_date': DateForm(),
-            'q':request.GET["q"],
-            'page_obj':page_obj,
-            'base': request.GET['q']
-        }
-
-    return render(request, 'webapp_main/articles_list.html', context)
-
-@login_required
-def sportsListView(request, source, topic):
-    if source == 'all':
-        if topic != 'others':
-            article_list=Articles.objects.filter(tag__icontains=topic, date=day())
-        else:
-            article_list=Articles.objects.exclude(Q(tag__icontains="News")|Q(tag="Sports")|Q(tag__icontains="Letters")).filter(date=day()).order_by('tag')
-
-    elif source[:1].isdigit() and '-' in source:
-        if topic != 'others':
-            article_list=Articles.objects.filter(tag__icontains=topic, date=source)
-        else:
-            article_list=Articles.objects.exclude(Q(tag__icontains="News")|Q(tag="Sports")|Q(tag__icontains="Letters")).filter(date=source).order_by('tag')
+    if requestedTag:
+        article_list =  Articles.objects.filter(date = requestedDate, tag__icontains = requestedTag).order_by('source') if requestedTag != "others" else excludedItems.filter(date = requestedDate).order_by('tag')
+        queryString = "q={}&tag={}".format(requestedDate, requestedTag)
     else:
-        if topic != 'others':
-            article_list=Articles.objects.filter(source__icontains=source, tag__icontains=topic, date=day())
-        else:
-            article_list=Articles.objects.exclude(Q(tag__icontains="News")|Q(tag="Sports")|Q(tag__icontains="Letters")).filter(source__icontains=source, date=day()).order_by('tag')
+        article_list=Articles.objects.filter(date = requestedDate).order_by('source')
+        queryString = "q={}".format(requestedDate)
 
-    paginator=Paginator(article_list, 8)
-    p_num=request.GET.get('page')
-    page_obj=paginator.get_page(p_num)
+    paginator = Paginator(article_list, 8)
+    p_num = request.GET.get('page') if request.GET.get('page') else 1
+    page_obj = paginator.get_page(p_num)
 
-    context={
+    context = {
         'articles':page_obj.object_list,
-        'title':source.upper(),
-        'topic': source+' - '+topic,
-        #'form':SearchForm(),
+        'title': requestedDate +' Stories',
+        'topic':'News From ' + requestedDate,
         'page_obj':page_obj,
-        'base': source
-
+        'source': 'archive',
+        'q' : requestedDate,
+        'query': queryString
     }
 
     return render(request, 'webapp_main/articles_list.html', context)
 
-@login_required
-def source(request, source):
-    article_list=Articles.objects.filter(source__icontains=source, date=day())
-    paginator=Paginator(article_list, 8)
-    p_num=request.GET.get('page')
-    page_obj=paginator.get_page(p_num)
-
-    context={
-        'articles':page_obj.object_list,
-        'title':source.upper(),
-        'topic':source+' news',
-        #'form':SearchForm(),
-        'page_obj':page_obj,
-        'base': source
-    }
-
-    return render(request, 'webapp_main/articles_list.html', context)
-
+## Class Based Views
 class ArticleDetailView(LoginRequiredMixin, DetailView):
     model=Articles
     context_object_name='article'
 
     query_pk_and_slug = True
-    
-@login_required
-def SearchView(request):
-    if request.method == 'GET':
-        article_list=Articles.objects.filter(title__icontains=request.GET["q"]).order_by('source')
-
-        paginator=Paginator(article_list, 8)
-        p_num=request.GET.get('page')
-        page_obj=paginator.get_page(p_num)
-
-        context={
-            'articles':page_obj.object_list,
-            'title': request.GET["q"]+' Results',
-            'topic':'Results For "'+request.GET["q"]+'"',
-            #'form':SearchForm(),
-            #'form_date': DateForm(),
-            'q':request.GET["q"],
-            'page_obj':page_obj,
-            'base': 'search/'+request.GET["q"]
-        }
-
-    return render(request, 'webapp_main/articles_list.html', context)
-
-@login_required
-def SearchView_Filter(request, query, topic):
-    if request.method == 'GET':
-        if topic != 'others':
-            article_list=Articles.objects.filter(tag__icontains=topic, title__icontains=query)
-        else:
-            article_list=Articles.objects.exclude(Q(tag__icontains="News")|Q(tag="Sports")|Q(tag__icontains="Letters")).filter(tag__icontains=topic, title__icontains=query).order_by('tag')
-
-        paginator=Paginator(article_list, 8)
-        p_num=request.GET.get('page')
-        page_obj=paginator.get_page(p_num)
-
-        context={
-            'articles':page_obj.object_list,
-            'title': query+' Results',
-            'topic':'Results For "'+query+'"',
-            #'form':SearchForm(),
-            #'form_date': DateForm(),
-            'q':query,
-            'page_obj':page_obj,
-            'base': 'search/'+query
-        }
-
-    return render(request, 'webapp_main/articles_list.html', context)
